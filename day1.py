4f10508c0c0b63a98ee088ced016e11a968de2d9
CACpETHT <erik.corecovers@gmail.com>
2026-05-11 18:32:02 -0700

    refactor: clean up commented code and enhance band name generator functionality

--- day1.py
+++ day1.py
@@ -10,7 +10,14 @@
 #print("hello " + input("What is your name") + "!")
 
 #variables Ej4
-name = input("What is your name ")
-#name = "Erik"
-print(name)
-print(len(name))
+# name = input("What is your name ")
+# #name = "Erik"
+# print(name)
+# print(len(name))
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in ? \n")
+#print(city)
+petName = input("What's your pet's name? \n")
+#print(petName)
+print("Your band name could be " + city + " " + petName)
