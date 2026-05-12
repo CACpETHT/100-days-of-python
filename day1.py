60c09e34e845e5c0692a2b509c7a9a53d45f1e8f
CACpETHT <erik.corecovers@gmail.com>
2026-05-11 18:15:05 -0700

    primer commit

---
+++ day1.py
@@ -0,0 +1,16 @@
+#print en consola ej.1
+# print("hello python")
+# print("hello python \nhello python")
+
+#concatenar Ej.2
+#print("hello" + " " +  "Erik")
+
+
+#inputs Ej.3
+#print("hello " + input("What is your name") + "!")
+
+#variables Ej4
+name = input("What is your name ")
+#name = "Erik"
+print(name)
+print(len(name))
